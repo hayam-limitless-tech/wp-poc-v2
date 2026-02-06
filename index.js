13bd631b276c257933fbb6273ca9502310fe6aef
Hayam <hayam@limitless-tech.ai>
2026-02-06 21:49:31 +0200

    im done

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import express from "express";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("OK");
-});
+const PORT = process.env.PORT;
+
+if (!PORT) {
+  console.error("PORT not set");
+  process.exit(1);
+}
+
+app.get("/", (req, res) => res.send("OK"));
 
 app.listen(PORT, () => {
   console.log("Listening on port", PORT);
